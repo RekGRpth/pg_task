fa319c78fe7fc7360b728356f75c959f705a7b37
RekGRpth <rekgrpth@gmail.com>
2019-02-25 12:30:59 +0500

    up

--- src/pg_scheduler.c
+++ src/pg_scheduler.c
@@ -17,11 +17,13 @@
 PG_MODULE_MAGIC;
 
 void _PG_init(void);
-void ticker(Datum arg);
-void runner(Datum arg);
+
+void man(Datum arg);
+void task(Datum arg);
 
 static volatile sig_atomic_t got_sighup = false;
 static volatile sig_atomic_t got_sigterm = false;
+
 static char *database = NULL;
 static char *username = NULL;
 static int period = 1;
@@ -72,13 +74,13 @@ static inline void finish_my() {
     (void)ProcessCompletedNotifies();
 }
 
-static inline void expirer() {
+/*static inline void expirer() {
     (void)connect_my();
     if (execute_my("UPDATE task SET state = 'EXPIRE' WHERE state = 'QUEUE' AND duration IS NOT NULL AND dt + duration < now()") != SPI_OK_UPDATE) elog(FATAL, "execute_my != SPI_OK_UPDATE");
     (void)finish_my();
-}
+}*/
 
-static inline void launch_runner(Datum id) {
+static inline void launch_task(Datum id) {
     BackgroundWorker worker;
     BackgroundWorkerHandle *handle;
     pid_t pid;
@@ -89,10 +91,10 @@ static inline void launch_runner(Datum id) {
     worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
     worker.bgw_restart_time = BGW_NEVER_RESTART;
     if (snprintf(worker.bgw_library_name, sizeof("pg_scheduler"), "pg_scheduler") != sizeof("pg_scheduler") - 1) elog(FATAL, "snprintf");
-    if (snprintf(worker.bgw_function_name, sizeof("runner"), "runner") != sizeof("runner") - 1) elog(FATAL, "snprintf");
-    len = sizeof("%s %s pg_scheduler runner") - 1 + strlen(database) - 1 + strlen(username) - 1 - 2;
-    if (snprintf(worker.bgw_name, len + 1, "%s %s pg_scheduler runner", database, username) != len) elog(FATAL, "snprintf");
-    if (snprintf(worker.bgw_type, sizeof("pg_scheduler runner"), "pg_scheduler runner") != sizeof("pg_scheduler runner") - 1) elog(FATAL, "snprintf");
+    if (snprintf(worker.bgw_function_name, sizeof("task"), "task") != sizeof("task") - 1) elog(FATAL, "snprintf");
+    len = sizeof("%s %s pg_scheduler task") - 1 + strlen(database) - 1 + strlen(username) - 1 - 2;
+    if (snprintf(worker.bgw_name, len + 1, "%s %s pg_scheduler task", database, username) != len) elog(FATAL, "snprintf");
+    if (snprintf(worker.bgw_type, sizeof("pg_scheduler task"), "pg_scheduler task") != sizeof("pg_scheduler task") - 1) elog(FATAL, "snprintf");
     worker.bgw_notify_pid = MyProcPid;
     worker.bgw_main_arg = id;
     oldcontext = MemoryContextSwitchTo(TopMemoryContext);
@@ -101,38 +103,48 @@ static inline void launch_runner(Datum id) {
     switch (WaitForBackgroundWorkerStartup(handle, &pid)) {
         case BGWH_STARTED: break;
         case BGWH_STOPPED:
-//            (void)pfree(handle);
+            (void)pfree(handle);
             ereport(ERROR, (errcode(ERRCODE_INSUFFICIENT_RESOURCES), errmsg("could not start background process"), errhint("More details may be available in the server log.")));
             break;
         case BGWH_POSTMASTER_DIED:
-//            (void)pfree(handle);
+            (void)pfree(handle);
             ereport(ERROR, (errcode(ERRCODE_INSUFFICIENT_RESOURCES), errmsg("cannot start background processes without postmaster"), errhint("Kill all remaining database processes and restart the database.")));
             break;
         default: elog(ERROR, "unexpected bgworker handle status"); break;
     }
-    if (handle != NULL) (void)pfree(handle);
+//    if (handle != NULL) (void)pfree(handle);
 }
 
-static inline void assigner() {
+/*static inline void assigner() {
     bool isnull;
     (void)connect_my();
     if (execute_my("UPDATE task SET state = 'ASSIGN' WHERE state = 'QUEUE' AND dt <= now() RETURNING id") != SPI_OK_UPDATE_RETURNING) elog(FATAL, "execute_my != SPI_OK_UPDATE_RETURNING");
-    for (unsigned int i = 0; i < SPI_processed; i++) (void)launch_runner(SPI_getbinval(SPI_tuptable->vals[i], SPI_tuptable->tupdesc, 1, &isnull));
+    for (unsigned int i = 0; i < SPI_processed; i++) (void)launch_task(SPI_getbinval(SPI_tuptable->vals[i], SPI_tuptable->tupdesc, 1, &isnull));
     (void)finish_my();
-}
+}*/
 
-void runner(Datum main_arg) {
+void task(Datum main_arg) {
     Oid argtypes[] = {INT8OID};
     Datum Values[] = {main_arg};
+    elog(LOG, "task started id=%li", DatumGetInt64(main_arg));
     (void)BackgroundWorkerUnblockSignals();
     (void)BackgroundWorkerInitializeConnection(database, username, 0);
     (void)connect_my();
-    if (execute_with_args_my("SELECT * FROM task WHERE id = $1", 1, argtypes, Values, NULL) != SPI_OK_SELECT) elog(FATAL, "execute_my != SPI_OK_SELECT");
+//    if (execute_with_args_my("SELECT * FROM task WHERE id = $1", 1, argtypes, Values, NULL) != SPI_OK_SELECT) elog(FATAL, "execute_my != SPI_OK_SELECT");
+    if (execute_with_args_my("UPDATE task SET state = 'WORK' WHERE id = $1 RETURNING request", 1, argtypes, Values, NULL) != SPI_OK_UPDATE_RETURNING) elog(FATAL, "execute_my != SPI_OK_UPDATE_RETURNING");
+//    elog(LOG, "SPI_processed=%li", SPI_processed);
+    for (unsigned int i = 0; i < SPI_processed; i++) {
+        char *request = SPI_getvalue(SPI_tuptable->vals[i], SPI_tuptable->tupdesc, 1);
+        elog(LOG, "request=%s", request);
+        (int)execute_my(request);
+        if (request != NULL) (void)pfree(request);
+        if (execute_with_args_my("UPDATE task SET state = 'DONE' WHERE id = $1", 1, argtypes, Values, NULL) != SPI_OK_UPDATE) elog(FATAL, "execute_my != SPI_OK_UPDATE");
+    }
     (void)finish_my();
 }
 
-void ticker(Datum main_arg) {
-    elog(LOG, "ticker started database=%s, username=%s", database, username);
+void man(Datum main_arg) {
+    elog(LOG, "man started database=%s, username=%s", database, username);
     pqsignal(SIGHUP, sighup);
     pqsignal(SIGTERM, sigterm);
     (void)BackgroundWorkerUnblockSignals();
@@ -147,8 +159,16 @@ void ticker(Datum main_arg) {
             (void)ProcessConfigFile(PGC_SIGHUP);
         }
         if (rc & WL_TIMEOUT) {
-            expirer();
-            assigner();
+//            expirer();
+//            assigner();
+            bool isnull;
+            (void)connect_my();
+            if (execute_my("UPDATE task SET state = 'ASSIGN' WHERE state = 'QUEUE' AND dt <= now() RETURNING id") != SPI_OK_UPDATE_RETURNING) elog(FATAL, "execute_my != SPI_OK_UPDATE_RETURNING");
+            for (unsigned int i = 0; i < SPI_processed; i++) {
+                elog(LOG, "i=%i", i);
+                (void)launch_task(SPI_getbinval(SPI_tuptable->vals[i], SPI_tuptable->tupdesc, 1, &isnull));
+            }
+            (void)finish_my();
         }
     }
     (void)proc_exit(1);
@@ -162,16 +182,16 @@ void _PG_init(void) {
     MemSet(&worker, 0, sizeof(BackgroundWorker));
     (void)DefineCustomStringVariable("pg_scheduler.database", "pg_scheduler database", NULL, &database, "postgres", PGC_POSTMASTER, 0, NULL, NULL, NULL);
     (void)DefineCustomStringVariable("pg_scheduler.username", "pg_scheduler username", NULL, &username, "postgres", PGC_POSTMASTER, 0, NULL, NULL, NULL);
-    (void)DefineCustomIntVariable("pg_scheduler.period", "how often to run ticker", NULL, &period, 1, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
-    (void)DefineCustomIntVariable("pg_scheduler.restart", "how often to restart ticker", NULL, &worker.bgw_restart_time, 10, 1, INT_MAX, PGC_POSTMASTER, 0, NULL, NULL, NULL);
+    (void)DefineCustomIntVariable("pg_scheduler.period", "how often to run man", NULL, &period, 1, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    (void)DefineCustomIntVariable("pg_scheduler.restart", "how often to restart man", NULL, &worker.bgw_restart_time, 10, 1, INT_MAX, PGC_POSTMASTER, 0, NULL, NULL, NULL);
     worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
     worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
 //    worker.bgw_restart_time = 10;
     if (snprintf(worker.bgw_library_name, sizeof("pg_scheduler"), "pg_scheduler") != sizeof("pg_scheduler") - 1) elog(FATAL, "snprintf");
-    if (snprintf(worker.bgw_function_name, sizeof("ticker"), "ticker") != sizeof("ticker") - 1) elog(FATAL, "snprintf");
-    len = sizeof("%s %s pg_scheduler ticker") - 1 + strlen(database) - 1 + strlen(username) - 1 - 2;
-    if (snprintf(worker.bgw_name, len + 1, "%s %s pg_scheduler ticker", database, username) != len) elog(FATAL, "snprintf");
-    if (snprintf(worker.bgw_type, sizeof("pg_scheduler ticker"), "pg_scheduler ticker") != sizeof("pg_scheduler ticker") - 1) elog(FATAL, "snprintf");
+    if (snprintf(worker.bgw_function_name, sizeof("man"), "man") != sizeof("man") - 1) elog(FATAL, "snprintf");
+    len = sizeof("%s %s pg_scheduler man") - 1 + strlen(database) - 1 + strlen(username) - 1 - 2;
+    if (snprintf(worker.bgw_name, len + 1, "%s %s pg_scheduler man", database, username) != len) elog(FATAL, "snprintf");
+    if (snprintf(worker.bgw_type, sizeof("pg_scheduler man"), "pg_scheduler man") != sizeof("pg_scheduler man") - 1) elog(FATAL, "snprintf");
     worker.bgw_notify_pid = 0;
     worker.bgw_main_arg = (Datum) 0;
     (void)RegisterBackgroundWorker(&worker);
