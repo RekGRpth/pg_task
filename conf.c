0889b2c060ece61ef7f845715ccb4df967e768c4
RekGRpth <rekgrpth@gmail.com>
2020-01-31 15:32:00 +0500

    up

--- conf.c
+++ conf.c
@@ -1,11 +1,74 @@
 #include "include.h"
 
+PG_MODULE_MAGIC;
+
 static volatile sig_atomic_t got_sighup = false;
 static volatile sig_atomic_t got_sigterm = false;
 
 static char *database;
 static char *table;
 static int32 period;
+
+void SPI_connect_my(const char *command, const int timeout) {
+    int rc;
+    pgstat_report_activity(STATE_RUNNING, command);
+    if ((rc = SPI_connect_ext(SPI_OPT_NONATOMIC)) != SPI_OK_CONNECT) ereport(ERROR, (errmsg("%s(%s:%d): SPI_connect_ext = %s", __func__, __FILE__, __LINE__, SPI_result_code_string(rc))));
+    SPI_start_transaction();
+    if (timeout > 0) enable_timeout_after(STATEMENT_TIMEOUT, timeout); else disable_timeout(STATEMENT_TIMEOUT, false);
+}
+
+void SPI_finish_my(const char *command) {
+    int rc;
+    disable_timeout(STATEMENT_TIMEOUT, false);
+    if ((rc = SPI_finish()) != SPI_OK_FINISH) ereport(ERROR, (errmsg("%s(%s:%d): SPI_finish = %s", __func__, __FILE__, __LINE__, SPI_result_code_string(rc))));
+    ProcessCompletedNotifies();
+    pgstat_report_activity(STATE_IDLE, command);
+    pgstat_report_stat(true);
+}
+
+static void register_conf_worker(void) {
+//    uint32 database_len = strlen(database), table_len = strlen(table), period_len = sizeof(int32);
+    StringInfoData buf;
+    BackgroundWorker worker;
+    initStringInfo(&buf);
+    MemSet(&worker, 0, sizeof(BackgroundWorker));
+    worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
+    worker.bgw_main_arg = (Datum) 0;
+    worker.bgw_notify_pid = 0;
+    worker.bgw_restart_time = BGW_DEFAULT_RESTART_INTERVAL;
+    worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
+    appendStringInfoString(&buf, "pg_task");
+    if (buf.len + 1 > BGW_MAXLEN) ereport(ERROR, (errmsg("%s(%s:%d): %u > BGW_MAXLEN", __func__, __FILE__, __LINE__, buf.len + 1)));
+    memcpy(worker.bgw_library_name, buf.data, buf.len);
+    resetStringInfo(&buf);
+    appendStringInfoString(&buf, "conf_worker");
+    if (buf.len + 1 > BGW_MAXLEN) ereport(ERROR, (errmsg("%s(%s:%d): %u > BGW_MAXLEN", __func__, __FILE__, __LINE__, buf.len + 1)));
+    memcpy(worker.bgw_function_name, buf.data, buf.len);
+    resetStringInfo(&buf);
+    appendStringInfoString(&buf, "pg_task conf");
+    if (buf.len + 1 > BGW_MAXLEN) ereport(ERROR, (errmsg("%s(%s:%d): %u > BGW_MAXLEN", __func__, __FILE__, __LINE__, buf.len + 1)));
+    memcpy(worker.bgw_type, buf.data, buf.len);
+    resetStringInfo(&buf);
+    appendStringInfoString(&buf, "postgres postgres pg_task conf");
+    if (buf.len + 1 > BGW_MAXLEN) ereport(ERROR, (errmsg("%s(%s:%d): %u > BGW_MAXLEN", __func__, __FILE__, __LINE__, buf.len + 1)));
+    memcpy(worker.bgw_name, buf.data, buf.len);
+    pfree(buf.data);
+//    if (database_len + 1 + table_len + 1 + period_len > BGW_EXTRALEN) ereport(ERROR, (errmsg("%s(%s:%d): %u > BGW_EXTRALEN", __func__, __FILE__, __LINE__, database_len + 1 + table_len + 1 + period_len)));
+//    memcpy(worker.bgw_extra, database, database_len);
+//    memcpy(worker.bgw_extra + database_len + 1, table, table_len);
+//    *(uint32 *)(worker.bgw_extra + database_len + 1 + table_len + 1) = period;
+    RegisterBackgroundWorker(&worker);
+}
+
+void _PG_init(void); void _PG_init(void) {
+    if (IsBinaryUpgrade) return;
+    if (!process_shared_preload_libraries_in_progress) ereport(FATAL, (errmsg("%s(%s:%d): !process_shared_preload_libraries_in_progress", __func__, __FILE__, __LINE__)));
+    DefineCustomStringVariable("pg_task.database", "pg_task database", NULL, &database, NULL, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_task.table", "pg_task table", "task", &table, NULL, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomIntVariable("pg_task.period", "pg_task period", NULL, &period, 1000, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    elog(LOG, "%s(%s:%d): database = %s, table = %s, period = %d", __func__, __FILE__, __LINE__, database ? database : "(null)", table, period);
+    register_conf_worker();
+}
 
 static void register_tick_worker(const char *database, const char *username, const char *schema, const char *table, int32 period) {
     StringInfoData buf;
@@ -110,9 +173,9 @@ static void sigterm(SIGNAL_ARGS) {
 }
 
 void conf_worker(Datum main_arg); void conf_worker(Datum main_arg) {
-    database = MyBgworkerEntry->bgw_extra;
-    table = database + strlen(database) + 1;
-    period = *(uint32 *)(table + strlen(table) + 1);
+//    database = MyBgworkerEntry->bgw_extra;
+//    table = database + strlen(database) + 1;
+//    period = *(uint32 *)(table + strlen(table) + 1);
     elog(LOG, "%s(%s:%d): database = %s, table = %s, period = %d", __func__, __FILE__, __LINE__, database ? database : "(null)", table, period);
     pqsignal(SIGHUP, sighup);
     pqsignal(SIGTERM, sigterm);
