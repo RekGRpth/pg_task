71d1ae00a056501906bb8725b6285b7d6a2f2553
RekGRpth <rekgrpth@gmail.com>
2020-02-18 07:42:23 +0500

    up

--- guc.c
+++ guc.c
@@ -1,5 +1,5 @@
 #include "include.h"
 
 int set_config_option_my(const char *name, const char *value) {
-    return set_config_option(name, value, (superuser() ? PGC_SUSET : PGC_USERSET), PGC_S_SESSION, false ? GUC_ACTION_LOCAL : GUC_ACTION_SET, true, 0, false);
+    return set_config_option(name, value, (superuser() ? PGC_SUSET : PGC_USERSET), PGC_S_OVERRIDE, false ? GUC_ACTION_LOCAL : GUC_ACTION_SET, true, ERROR, false);
 }
