c16f74067d4f51e25bc585919d84b7306bcc92ba
RekGRpth <rekgrpth@gmail.com>
2020-02-20 08:32:33 +0500

    up

--- latch.c
+++ latch.c
@@ -26,6 +26,13 @@ int WaitLatchOrSocketMy(Latch *latch, Context **context, int wakeEvents, List **
     if (!WaitEventSetWait(set, timeout, &event, 1, wait_event_info)) ret |= WL_TIMEOUT; else {
         ret |= event.events & (WL_LATCH_SET | WL_POSTMASTER_DEATH | WL_SOCKET_MASK);
         *context = event.user_data;
+        if (ret & WL_LATCH_SET) L("WL_LATCH_SET");
+        if (ret & WL_SOCKET_READABLE) L("WL_SOCKET_READABLE");
+        if (ret & WL_SOCKET_WRITEABLE) L("WL_SOCKET_WRITEABLE");
+        if (ret & WL_TIMEOUT) L("WL_TIMEOUT");
+        if (ret & WL_POSTMASTER_DEATH) L("WL_POSTMASTER_DEATH");
+        if (ret & WL_EXIT_ON_PM_DEATH) L("WL_EXIT_ON_PM_DEATH");
+        if (ret & WL_SOCKET_CONNECTED) L("WL_SOCKET_CONNECTED");
     }
     FreeWaitEventSet(set);
     return ret;
