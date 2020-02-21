3b77c7f31e8fa271bde3f7c19b8a48720625a191
RekGRpth <rekgrpth@gmail.com>
2020-02-21 09:03:17 +0500

    up

--- fe-exec.c
+++ fe-exec.c
@@ -1,10 +1,5 @@
 #include "include.h"
 
 const char *PQftypeMy(const PGresult *res, int column_number) {
-    HeapTuple typeTuple = SearchSysCache1(TYPEOID, ObjectIdGetDatum(PQftype(res, column_number)));
-    const char *result;
-    if (!HeapTupleIsValid(typeTuple)) E("SPI_ERROR_TYPUNKNOWN");
-    result = NameStr(((Form_pg_type)GETSTRUCT(typeTuple))->typname);
-    ReleaseSysCache(typeTuple);
-    return result;
+    return SPI_gettype_my(ObjectIdGetDatum(PQftype(res, column_number)));
 }
