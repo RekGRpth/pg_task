ec7be9551ca325feeafbc88e07461e5093348ec7
RekGRpth <rekgrpth@gmail.com>
2020-02-21 08:45:02 +0500

    up

---
+++ dest.c
@@ -0,0 +1,64 @@
+#include "include.h"
+
+extern MemoryContext myMemoryContext;
+extern StringInfoData response;
+
+static const char *SPI_fname_my(TupleDesc tupdesc, int fnumber) {
+    if (fnumber > tupdesc->natts || !fnumber || fnumber <= FirstLowInvalidHeapAttributeNumber) E("SPI_ERROR_NOATTRIBUTE");
+    return NameStr((fnumber > 0 ? TupleDescAttr(tupdesc, fnumber - 1) : SystemAttributeDefinition(fnumber))->attname);
+}
+
+static char *SPI_getvalue_my2(TupleTableSlot *slot, TupleDesc tupdesc, int fnumber) {
+    Datum val;
+    bool isnull;
+    Oid foutoid;
+    bool typisvarlena;
+    if (fnumber > tupdesc->natts || !fnumber || fnumber <= FirstLowInvalidHeapAttributeNumber) E("SPI_ERROR_NOATTRIBUTE");
+    val = slot_getattr(slot, fnumber, &isnull);
+    if (isnull) return NULL;
+    getTypeOutputInfo(fnumber > 0 ? TupleDescAttr(tupdesc, fnumber - 1)->atttypid : (SystemAttributeDefinition(fnumber))->atttypid, &foutoid, &typisvarlena);
+    return OidOutputFunctionCall(foutoid, val);
+}
+
+static const char *SPI_gettype_my(TupleDesc tupdesc, int fnumber) {
+    HeapTuple typeTuple;
+    const char *result;
+    if (fnumber > tupdesc->natts || !fnumber || fnumber <= FirstLowInvalidHeapAttributeNumber) E("SPI_ERROR_NOATTRIBUTE");
+    typeTuple = SearchSysCache1(TYPEOID, ObjectIdGetDatum(fnumber > 0 ? TupleDescAttr(tupdesc, fnumber - 1)->atttypid : (SystemAttributeDefinition(fnumber))->atttypid));
+    if (!HeapTupleIsValid(typeTuple)) E("SPI_ERROR_TYPUNKNOWN");
+    result = NameStr(((Form_pg_type)GETSTRUCT(typeTuple))->typname);
+    ReleaseSysCache(typeTuple);
+    return result;
+}
+
+static bool receiveSlot(TupleTableSlot *slot, DestReceiver *self) {
+    MemoryContext oldMemoryContext = MemoryContextSwitchTo(myMemoryContext);
+    if (!response.data) initStringInfo(&response);
+    if (!response.len && slot->tts_tupleDescriptor->natts > 1) {
+        for (int col = 1; col <= slot->tts_tupleDescriptor->natts; col++) {
+            if (col > 1) appendStringInfoString(&response, "\t");
+            appendStringInfo(&response, "%s::%s", SPI_fname_my(slot->tts_tupleDescriptor, col), SPI_gettype_my(slot->tts_tupleDescriptor, col));
+        }
+    }
+    if (response.len) appendStringInfoString(&response, "\n");
+    for (int col = 1; col <= slot->tts_tupleDescriptor->natts; col++) {
+        const char *value = SPI_getvalue_my2(slot, slot->tts_tupleDescriptor, col);
+        if (col > 1) appendStringInfoString(&response, "\t");
+        appendStringInfoString(&response, value ? value : "(null)");
+        if (value) pfree((void *)value);
+    }
+    MemoryContextSwitchTo(oldMemoryContext);
+    return true;
+}
+
+static void rStartup(DestReceiver *self, int operation, TupleDesc typeinfo) { }
+
+static void rShutdown(DestReceiver *self) { }
+
+static void rDestroy(DestReceiver *self) { }
+
+static const DestReceiver DestReceiverMy = {.receiveSlot = receiveSlot, .rStartup = rStartup, .rShutdown = rShutdown, .rDestroy = rDestroy, .mydest = DestDebug};
+
+DestReceiver *CreateDestReceiverMy(CommandDest dest) {
+    return dest == DestDebug ? unconstify(DestReceiver *, &DestReceiverMy) : CreateDestReceiver(dest);
+}
