4cfd0a1d21bb051fe4d47fda5c26f594e0569a40
RekGRpth <rekgrpth@gmail.com>
2020-02-27 13:53:29 +0500

    up

--- init.c
+++ init.c
@@ -27,10 +27,10 @@ void RegisterDynamicBackgroundWorker_my(BackgroundWorker *worker) {
     if (!RegisterDynamicBackgroundWorker(worker, &handle)) E("!RegisterDynamicBackgroundWorker"); else {
         pid_t pid;
         switch (WaitForBackgroundWorkerStartup(handle, &pid)) {
+            case BGWH_NOT_YET_STARTED: E("WaitForBackgroundWorkerStartup == BGWH_NOT_YET_STARTED");
+            case BGWH_POSTMASTER_DIED: E("WaitForBackgroundWorkerStartup == BGWH_POSTMASTER_DIED");
             case BGWH_STARTED: break;
             case BGWH_STOPPED: E("WaitForBackgroundWorkerStartup == BGWH_STOPPED");
-            case BGWH_POSTMASTER_DIED: E("WaitForBackgroundWorkerStartup == BGWH_POSTMASTER_DIED");
-            default: E("Unexpected bgworker handle status");
         }
     }
     pfree(handle);
