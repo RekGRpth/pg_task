e7270db7b50ea3dfcc9d66955be18be29c576b2e
RekGRpth <rekgrpth@gmail.com>
2021-03-13 12:35:20 +0500

    rename

--- init.c
+++ init.c
@@ -34,7 +34,7 @@ void init_escape(StringInfoData *buf, const char *data, int len, char escape) {
     }
 }
 
-static void conf_work(void) {
+static void init_work(void) {
     StringInfoData buf;
     BackgroundWorker worker;
     MemSet(&worker, 0, sizeof(worker));
@@ -61,14 +61,18 @@ static void conf_work(void) {
     RegisterBackgroundWorker(&worker);
 }
 
-void _PG_init(void); void _PG_init(void) {
-    if (IsBinaryUpgrade) { W("IsBinaryUpgrade"); return; }
-    if (!process_shared_preload_libraries_in_progress) F("!process_shared_preload_libraries_in_progress");
+static void init_conf(void) {
     DefineCustomIntVariable("pg_task.default_reset", "pg_task default reset", NULL, &default_reset, 60, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
     DefineCustomIntVariable("pg_task.default_timeout", "pg_task default timeout", NULL, &default_timeout, 1000, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.json", "pg_task json", NULL, &default_json, "[{\"data\":\"postgres\"}]", PGC_SIGHUP, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.default_null", "pg_task default null", NULL, &default_null, "\\N", PGC_SIGHUP, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.default_table", "pg_task default table", NULL, &default_table, "task", PGC_SIGHUP, 0, NULL, NULL, NULL);
     D1("json = %s, table = %s, null = %s, reset = %i, timeout = %i", default_json, default_table, default_null, default_reset, default_timeout);
-    conf_work();
+    init_work();
+}
+
+void _PG_init(void); void _PG_init(void) {
+    if (IsBinaryUpgrade) { W("IsBinaryUpgrade"); return; }
+    if (!process_shared_preload_libraries_in_progress) F("!process_shared_preload_libraries_in_progress");
+    init_conf();
 }
