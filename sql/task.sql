55e7d0c768b46f3966e7daf8e47b2f9f5b3cdffb
RekGRpth <rekgrpth@gmail.com>
2021-11-11 15:46:19 +0500

    test

--- sql/task.sql
+++ sql/task.sql
@@ -1,6 +1,20 @@
 \unset ECHO
-\i test_setup.sql
+\set QUIET 1
+-- Turn off echo and keep things quiet.
 
+-- Format the output for nice TAP.
+\pset format unaligned
+\pset tuples_only true
+\pset pager off
+
+-- Revert all changes on failure.
+\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_STOP true
+
+-- Load the TAP functions.
+BEGIN;
+--\i pgtap.sql
+CREATE EXTENSION pgtap;
 SELECT plan(2);
 SELECT has_schema(current_setting('pg_task.default_schema', false)::name);
 SELECT has_table(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name);
