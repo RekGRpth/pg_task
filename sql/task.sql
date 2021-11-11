719d460688da26d0bd1223dc50447243db9e8e33
RekGRpth <rekgrpth@gmail.com>
2021-11-11 15:39:02 +0500

    test

--- sql/task.sql
+++ sql/task.sql
@@ -1,7 +1,8 @@
 \unset ECHO
 \i test_setup.sql
 
-SELECT plan(1);
-SELECT pass('W00t!');
-SELECT * FROM finish();
+SELECT plan(2);
+SELECT has_schema(current_setting('pg_task.default_schema', false)::name);
+SELECT has_table(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name);
+SELECT * FROM finish(true);
 ROLLBACK;
