b98db12829cf53ed09d643c24af84537359f8d3e
RekGRpth <rekgrpth@gmail.com>
2021-11-11 15:53:40 +0500

    test

--- sql/task.sql
+++ sql/task.sql
@@ -15,8 +15,9 @@
 BEGIN;
 --\i pgtap.sql
 CREATE EXTENSION pgtap;
-SELECT plan(2);
+SELECT plan(3);
 SELECT has_schema(current_setting('pg_task.default_schema', false)::name);
+SELECT has_enum(current_setting('pg_task.default_schema', false)::name, 'state'::name);
 SELECT has_table(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name);
 SELECT * FROM finish(true);
 ROLLBACK;
