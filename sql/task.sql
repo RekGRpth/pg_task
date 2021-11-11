cefa3256dc1d5852a845181e9384a1aabd34d19b
RekGRpth <rekgrpth@gmail.com>
2021-11-11 16:16:50 +0500

    test

--- sql/task.sql
+++ sql/task.sql
@@ -15,11 +15,12 @@
 BEGIN;
 --\i pgtap.sql
 CREATE EXTENSION pgtap;
-SELECT plan(5);
+SELECT plan(6);
 SELECT has_schema(current_setting('pg_task.default_schema', false)::name);
 SELECT has_enum(current_setting('pg_task.default_schema', false)::name, 'state'::name);
 SELECT enum_has_labels(current_setting('pg_task.default_schema', false)::name, 'state'::name, ARRAY['PLAN', 'TAKE', 'WORK', 'DONE', 'FAIL', 'STOP']);
 SELECT has_table(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name);
-SELECT has_column(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name, 'id'::name, 'Column ' || quote_ident(current_setting('pg_task.default_schema', false)) || '.' || quote_ident('id') || ' should exist');
+SELECT has_column(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name, 'id'::name, 'Column ' || quote_ident(current_setting('pg_task.default_table', false)) || '.' || quote_ident('id') || ' should exist');
+SELECT has_column(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name, 'parent'::name, 'Column ' || quote_ident(current_setting('pg_task.default_table', false)) || '.' || quote_ident('parent') || ' should exist');
 SELECT * FROM finish();
 ROLLBACK;
