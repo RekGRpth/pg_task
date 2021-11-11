26be5ccde0f28edbb8141dd7483692db4bf735c2
RekGRpth <rekgrpth@gmail.com>
2021-11-11 16:08:16 +0500

    test

--- sql/task.sql
+++ sql/task.sql
@@ -15,10 +15,11 @@
 BEGIN;
 --\i pgtap.sql
 CREATE EXTENSION pgtap;
-SELECT plan(4);
+SELECT plan(5);
 SELECT has_schema(current_setting('pg_task.default_schema', false)::name);
 SELECT has_enum(current_setting('pg_task.default_schema', false)::name, 'state'::name);
 SELECT enum_has_labels(current_setting('pg_task.default_schema', false)::name, 'state'::name, ARRAY['PLAN', 'TAKE', 'WORK', 'DONE', 'FAIL', 'STOP']);
 SELECT has_table(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name);
-SELECT * FROM finish(true);
+SELECT has_column(current_setting('pg_task.default_schema', false)::name, current_setting('pg_task.default_table', false)::name, 'id'::name, 'Column ' || quote_ident(current_setting('pg_task.default_schema', false)) || '.' || quote_ident('id') || ' should exist');
+SELECT * FROM finish();
 ROLLBACK;
