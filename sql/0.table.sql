e7cab481ee50ed70da3e85b392e869dc9215bab4
RekGRpth <rekgrpth@gmail.com>
2021-11-12 16:37:53 +0500

    test

--- sql/0.table.sql
+++ sql/0.table.sql
@@ -1,19 +1,12 @@
 \unset ECHO
 \set QUIET 1
--- Turn off echo and keep things quiet.
-
--- Format the output for nice TAP.
 \pset format unaligned
 \pset tuples_only true
 \pset pager off
-
--- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
-
--- Load the TAP functions.
 BEGIN;
---\i pgtap.sql
+
 CREATE EXTENSION pgtap;
 
 SELECT plan(165);
--- sql/1.local.sql
+++ sql/1.local.sql
@@ -1,16 +1,10 @@
 \unset ECHO
 \set QUIET 1
--- Turn off echo and keep things quiet.
-
--- Format the output for nice TAP.
 \pset format unaligned
 \pset tuples_only true
 \pset pager off
-
--- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
-
 
 BEGIN;
 
@@ -19,9 +13,9 @@ INSERT INTO task (input) VALUES ('SELECT 1');
 COMMIT;
 
 SELECT pg_sleep(2);
--- Load the TAP functions.
+
 BEGIN;
---\i pgtap.sql
+
 CREATE EXTENSION pgtap;
 
 SELECT plan(1);
