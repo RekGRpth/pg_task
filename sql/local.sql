b6bca8b29fb5abbdb94b5ef75d48f7d735d22336
RekGRpth <rekgrpth@gmail.com>
2021-11-15 10:26:35 +0500

    test

--- sql/local.sql
+++ sql/local.sql
@@ -9,17 +9,17 @@
 SELECT quote_literal(CURRENT_TIMESTAMP) AS ct
 \gset
 
-INSERT INTO task (input) VALUES ('SELECT 1 AS a0 WHERE false');
+INSERT INTO task ("group", input) VALUES ('0', 'SELECT 1 AS a WHERE false');
 
-INSERT INTO task (input) VALUES ('SELECT 1/0 AS a1');
+INSERT INTO task ("group", input) VALUES ('1', 'SELECT 1/0 AS a');
 
-INSERT INTO task (input) VALUES ('SELECT 1 AS a2');
-INSERT INTO task (input) VALUES ('SELECT 1 AS a3, 2 AS b3');
-INSERT INTO task (input) VALUES ('SELECT 1 AS a4;SELECT 2 AS b4');
-INSERT INTO task (input) VALUES ('SELECT 1 AS a5, 2 AS b5;SELECT 3 AS c5');
-INSERT INTO task (input) VALUES ('SELECT 1 AS a6, 2 AS b6;SELECT 3 AS c6, 4 AS d6');
+INSERT INTO task ("group", input) VALUES ('2', 'SELECT 1 AS a');
+INSERT INTO task ("group", input) VALUES ('3', 'SELECT 1 AS a, 2 AS b');
+INSERT INTO task ("group", input) VALUES ('4', 'SELECT 1 AS a;SELECT 2 AS b');
+INSERT INTO task ("group", input) VALUES ('5', 'SELECT 1 AS a, 2 AS b;SELECT 3 AS c');
+INSERT INTO task ("group", input) VALUES ('6', 'SELECT 1 AS a, 2 AS b;SELECT 3 AS c, 4 AS d');
 
-WITH s AS (SELECT generate_series(1,10) AS s)  INSERT INTO task (input, max, live) SELECT 'SELECT pg_sleep(0.1) AS a7', 2, '1 min' FROM s;
+WITH s AS (SELECT generate_series(1,10) AS s)  INSERT INTO task ("group", input, max, live) SELECT '7', 'SELECT pg_sleep(0.1) AS a', 2, '1 min' FROM s;
 
 DO $body$ <<local>> DECLARE
     count bigint;
@@ -33,14 +33,14 @@ BEGIN
     END LOOP;
 END;$body$ LANGUAGE plpgsql;
 
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a0 WHERE false' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '0' AND input = 'SELECT 1 AS a WHERE false' AND start >= :ct::timestamp ORDER BY id desc;
 
-SELECT input, output, state FROM task WHERE input = 'SELECT 1/0 AS a1' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '1' AND input = 'SELECT 1/0 AS a' AND start >= :ct::timestamp ORDER BY id desc;
 
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a2' AND start >= :ct::timestamp ORDER BY id desc;
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a3, 2 AS b3' AND start >= :ct::timestamp ORDER BY id desc;
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a4;SELECT 2 AS b4' AND start >= :ct::timestamp ORDER BY id desc;
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a5, 2 AS b5;SELECT 3 AS c5' AND start >= :ct::timestamp ORDER BY id desc;
-SELECT input, output, state FROM task WHERE input = 'SELECT 1 AS a6, 2 AS b6;SELECT 3 AS c6, 4 AS d6' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '2' AND input = 'SELECT 1 AS a' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '3' AND input = 'SELECT 1 AS a, 2 AS b' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '4' AND input = 'SELECT 1 AS a;SELECT 2 AS b' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '5' AND input = 'SELECT 1 AS a, 2 AS b;SELECT 3 AS c' AND start >= :ct::timestamp ORDER BY id desc;
+SELECT "group", input, output, state FROM task WHERE "group" = '6' AND input = 'SELECT 1 AS a, 2 AS b;SELECT 3 AS c, 4 AS d' AND start >= :ct::timestamp ORDER BY id desc;
 
-SELECT input, output, state, count(id) FROM task WHERE input like 'SELECT pg_sleep(0.1) AS a7' AND start >= :ct::timestamp GROUP BY input, output, state, pid;
+SELECT "group", input, output, state, count(id) FROM task WHERE "group" = '7' AND input like 'SELECT pg_sleep(0.1) AS a' AND start >= :ct::timestamp GROUP BY "group", input, output, state, pid;
