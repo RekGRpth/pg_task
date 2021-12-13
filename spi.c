fff166b17bef4c5efe9ca2dd8c0d517a5064fde7
RekGRpth <rekgrpth@gmail.com>
2021-12-13 12:57:21 +0500

    error

--- spi.c
+++ spi.c
@@ -50,7 +50,7 @@ void SPI_execute_plan_my(SPIPlanPtr plan, Datum *values, const char *nulls, int 
 
 void SPI_execute_with_args_my(const char *src, int nargs, Oid *argtypes, Datum *values, const char *nulls, int res, bool commit) {
     int rc;
-    if ((rc = SPI_execute_with_args(src, nargs, argtypes, values, nulls, false, 0)) != res) E("%s != %s", SPI_result_code_string(res), SPI_result_code_string(rc));
+    if ((rc = SPI_execute_with_args(src, nargs, argtypes, values, nulls, false, 0)) != res) elog(ERROR, "SPI_execute_with_args failed: %s while expecting %s for %s", SPI_result_code_string(rc), SPI_result_code_string(res), src);
     if (commit) SPI_commit_my();
 }
 
