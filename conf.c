0208fa3426c6a44313c2fff6fd0a33c11613ba42
RekGRpth <rekgrpth@gmail.com>
2023-01-14 09:17:34 +0500

    quote

--- conf.c
+++ conf.c
@@ -35,7 +35,7 @@ void conf_main(Datum arg) {
     static const char *src = SQL(
         WITH _ AS (
             WITH _ AS (
-                SELECT "setdatabase", regexp_split_to_array(UNNEST("setconfig"), '=') AS "setconfig" FROM pg_db_role_setting
+                SELECT "setdatabase", regexp_split_to_array(UNNEST("setconfig"), '=') AS "setconfig" FROM "pg_db_role_setting"
             ) SELECT "setdatabase", jsonb_object(array_agg("setconfig"[1]), array_agg("setconfig"[2])) AS "setconfig" FROM _ GROUP BY 1
         ) SELECT    "datname" AS "data",
                     "usename" AS "user",
@@ -43,7 +43,7 @@ void conf_main(Datum arg) {
                     "setconfig"->>'pg_task.schema' AS "schema",
                     ("setconfig"->>'pg_task.sleep')::bigint AS "sleep",
                     "setconfig"->>'pg_task.table' AS "table"
-        FROM _ INNER JOIN pg_database ON "setdatabase" = "oid" INNER JOIN pg_user ON "usesysid" = "datdba"
+        FROM _ INNER JOIN "pg_database" ON "setdatabase" = "oid" INNER JOIN "pg_user" ON "usesysid" = "datdba"
         LEFT JOIN "pg_locks" AS l ON "locktype" = 'userlock' AND "mode" = 'AccessExclusiveLock' AND "granted" AND "objsubid" = 3 AND "database" = "oid" AND "classid" = "usesysid" AND "objid" = hashtext(quote_ident("setconfig"->>'pg_task.schema')||'.'||quote_ident("setconfig"->>'pg_task.table'))::oid
         WHERE "pid" IS NULL
     );
