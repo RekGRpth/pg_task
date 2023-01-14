3dc1baa9dfbad27328b17ca0957861bad4d4ead9
RekGRpth <rekgrpth@gmail.com>
2023-01-14 09:36:34 +0500

    op

--- conf.c
+++ conf.c
@@ -38,13 +38,13 @@ void conf_main(Datum arg) {
                 SELECT "setdatabase", regexp_split_to_array(UNNEST("setconfig"), '=') AS "setconfig" FROM "pg_db_role_setting"
             ) SELECT "setdatabase", jsonb_object(array_agg("setconfig"[1]), array_agg("setconfig"[2])) AS "setconfig" FROM _ GROUP BY 1
         ) SELECT    "datname" AS "data",
-                    "usename" AS "user",
+                    "rolname" AS "user",
                     EXTRACT(epoch FROM ("setconfig"->>'pg_task.reset')::interval)::bigint AS "reset",
                     "setconfig"->>'pg_task.schema' AS "schema",
                     ("setconfig"->>'pg_task.sleep')::bigint AS "sleep",
                     "setconfig"->>'pg_task.table' AS "table"
-        FROM _ INNER JOIN "pg_database" ON "setdatabase" = "oid" INNER JOIN "pg_user" ON "usesysid" = "datdba"
-        LEFT JOIN "pg_locks" ON "locktype" = 'userlock' AND "mode" = 'AccessExclusiveLock' AND "granted" AND "objsubid" = 3 AND "database" = "oid" AND "classid" = "usesysid" AND "objid" = hashtext(quote_ident("setconfig"->>'pg_task.schema')||'.'||quote_ident("setconfig"->>'pg_task.table'))::oid
+        FROM _ INNER JOIN "pg_database" ON "pg_database"."oid" = "setdatabase" INNER JOIN "pg_authid" ON "pg_authid"."oid" = "datdba"
+        LEFT JOIN "pg_locks" ON "locktype" = 'userlock' AND "mode" = 'AccessExclusiveLock' AND "granted" AND "objsubid" = 3 AND "database" = "setdatabase" AND "classid" = "datdba" AND "objid" = hashtext(quote_ident("setconfig"->>'pg_task.schema')||'.'||quote_ident("setconfig"->>'pg_task.table'))::oid
         WHERE "pid" IS NULL
     );
     BackgroundWorkerUnblockSignals();
