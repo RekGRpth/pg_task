e1b6b1a2257d7b38cddd37b75f9a5fd321c3e968
RekGRpth <rekgrpth@gmail.com>
2023-01-28 09:46:56 +0500

    -context

--- spi.c
+++ spi.c
@@ -11,7 +11,6 @@ Datum SPI_getbinval_my(HeapTupleData *tuple, TupleDesc tupdesc, const char *fnam
 Portal SPI_cursor_open_my(const char *name, SPIPlanPtr plan, Datum *values, const char *nulls) {
     Portal portal;
     if (!(portal = SPI_cursor_open(name, plan, values, nulls, false))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_cursor_open failed"), errdetail("%s", SPI_result_code_string(SPI_result))));
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
     return portal;
 }
@@ -19,7 +18,6 @@ Portal SPI_cursor_open_my(const char *name, SPIPlanPtr plan, Datum *values, cons
 Portal SPI_cursor_open_with_args_my(const char *name, const char *src, int nargs, Oid *argtypes, Datum *values, const char *nulls) {
     Portal portal;
     if (!(portal = SPI_cursor_open_with_args(name, src, nargs, argtypes, values, nulls, false, 0))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_cursor_open_with_args failed"), errdetail("%s", SPI_result_code_string(SPI_result)), errcontext("%s", src)));
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
     return portal;
 }
@@ -29,7 +27,6 @@ SPIPlanPtr SPI_prepare_my(const char *src, int nargs, Oid *argtypes) {
     SPIPlanPtr plan;
     if (!(plan = SPI_prepare(src, nargs, argtypes))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_prepare failed"), errdetail("%s", SPI_result_code_string(SPI_result)), errcontext("%s", src)));
     if ((rc = SPI_keepplan(plan))) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_keepplan failed"), errdetail("%s", SPI_result_code_string(rc)), errcontext("%s", src)));
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
     return plan;
 }
@@ -42,21 +39,18 @@ void SPI_connect_my(const char *src) {
     if ((rc = SPI_connect()) != SPI_OK_CONNECT) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_connect failed"), errdetail("%s", SPI_result_code_string(rc)), errcontext("%s", src)));
     PushActiveSnapshot(GetTransactionSnapshot());
     StatementTimeout > 0 ? enable_timeout_after(STATEMENT_TIMEOUT, StatementTimeout) : disable_timeout(STATEMENT_TIMEOUT, false);
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
 }
 
 void SPI_execute_plan_my(SPIPlanPtr plan, Datum *values, const char *nulls, int res) {
     int rc;
     if ((rc = SPI_execute_plan(plan, values, nulls, false, 0)) != res) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_execute_plan failed"), errdetail("%s while expecting %s", SPI_result_code_string(rc), SPI_result_code_string(res))));
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
 }
 
 void SPI_execute_with_args_my(const char *src, int nargs, Oid *argtypes, Datum *values, const char *nulls, int res) {
     int rc;
     if ((rc = SPI_execute_with_args(src, nargs, argtypes, values, nulls, false, 0)) != res) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_execute_with_args failed"), errdetail("%s while expecting %s", SPI_result_code_string(rc), SPI_result_code_string(res)), errcontext("%s", src)));
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
 }
 
@@ -71,6 +65,5 @@ void SPI_finish_my(void) {
     CommitTransactionCommand();
     pgstat_report_stat(false);
     pgstat_report_activity(STATE_IDLE, NULL);
-    MemoryContextSwitchTo(TopMemoryContext);
     CurrentResourceOwner = AuxProcessResourceOwner;
 }
