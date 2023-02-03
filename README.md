ef6ef3c9f76fa09b9e11b52fe1cf9fbd39d5f51c
RekGRpth <rekgrpth@gmail.com>
2023-02-03 17:21:32 +0500

    active

--- README.md
+++ README.md
@@ -51,7 +51,7 @@ INSERT INTO task (input, remote) VALUES ('SELECT now()', 'user=user host=host');
 | pg_task.table | text | task | config, database, user, session | Table name for tasks table |
 | pg_task.timeout | interval | 0 sec | config, database, user, session | Non-negative allowed time for task run |
 | pg_task.user | text | postgres | config | User name for tasks table |
-| pg_task.active | interval | 1 week | config, database, user, session | task active before now |
+| pg_work.active | interval | 1 week | config, database, user, session | Tasks filter interval for partitioning |
 
 `pg_task` creates table with folowing columns
 
--- init.c
+++ init.c
@@ -250,7 +250,7 @@ void _PG_init(void) {
     DefineCustomStringVariable("pg_task.table", "pg_task table", "Table name for tasks table", &task_table, "task", PGC_USERSET, 0, NULL, init_assign_table, NULL);
     DefineCustomStringVariable("pg_task.timeout", "pg_task timeout", "Non-negative allowed time for task run", &task_timeout, "0 sec", PGC_USERSET, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.user", "pg_task user", "User name for tasks table", &task_user, "postgres", PGC_SIGHUP, 0, NULL, init_assign_user, NULL);
-    DefineCustomStringVariable("pg_work.active", "pg_work active", "task active before now", &work_active, "1 week", PGC_USERSET, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_work.active", "pg_work active", "Tasks filter interval for partitioning", &work_active, "1 week", PGC_USERSET, 0, NULL, NULL, NULL);
     elog(DEBUG1, "json = %s, user = %s, data = %s, schema = %s, table = %s, null = %s, sleep = %i, reset = %s, active = %s", task_json, task_user, task_data, task_schema, task_table, task_null, task_sleep, task_reset, work_active);
 #ifdef GP_VERSION_NUM
     if (!IS_QUERY_DISPATCHER()) return;
