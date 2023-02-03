fdea981bba4635579098836bdbac609182995e07
RekGRpth <rekgrpth@gmail.com>
2023-02-03 17:11:23 +0500

    schema

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ INSERT INTO task (input, remote) VALUES ('SELECT now()', 'user=user host=host');
 | pg_task.quote | char | | config, database, user, session | Results columns quote |
 | pg_task.repeat | interval | 0 sec | config, database, user, session | Non-negative auto repeat tasks interval |
 | pg_task.reset | interval | 1 hour | config, database, user, session | Interval of reset tasks |
-| pg_task.schema | text | public | config, database, user, session | schema name for tasks table |
+| pg_task.schema | text | public | config, database, user, session | Schema name for tasks table |
 | pg_task.table | text | task | config, database, user, session | table name for tasks table |
 | pg_task.timeout | interval | 0 sec | config, database, user, session | task timeout |
 | pg_task.user | text | postgres | config | user name for tasks table |
--- init.c
+++ init.c
@@ -246,7 +246,7 @@ void _PG_init(void) {
     DefineCustomStringVariable("pg_task.quote", "pg_task quote", "Results columns quote", &task_quote, "", PGC_USERSET, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.repeat", "pg_task repeat", "Non-negative auto repeat tasks interval", &task_repeat, "0 sec", PGC_USERSET, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.reset", "pg_task reset", "Interval of reset tasks", &task_reset, "1 hour", PGC_USERSET, 0, NULL, init_assign_reset, NULL);
-    DefineCustomStringVariable("pg_task.schema", "pg_task schema", "schema name for tasks table", &task_schema, "public", PGC_USERSET, 0, NULL, init_assign_schema, NULL);
+    DefineCustomStringVariable("pg_task.schema", "pg_task schema", "Schema name for tasks table", &task_schema, "public", PGC_USERSET, 0, NULL, init_assign_schema, NULL);
     DefineCustomStringVariable("pg_task.table", "pg_task table", "table name for tasks table", &task_table, "task", PGC_USERSET, 0, NULL, init_assign_table, NULL);
     DefineCustomStringVariable("pg_task.timeout", "pg_task timeout", "task timeout", &task_timeout, "0 sec", PGC_USERSET, 0, NULL, NULL, NULL);
     DefineCustomStringVariable("pg_task.user", "pg_task user", "user name for tasks table", &task_user, "postgres", PGC_SIGHUP, 0, NULL, init_assign_user, NULL);
