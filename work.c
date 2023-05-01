e1ff12ac10b0a8da76b9b796f41161ac082171af
RekGRpth <rekgrpth@gmail.com>
2023-05-01 11:05:33 +0500

    fix pgspot Error: PS001: Unqualified operator: '>>'

--- work.c
+++ work.c
@@ -246,7 +246,7 @@ static void work_reset(void) {
         initStringInfoMy(&src);
         appendStringInfo(&src, SQL(
             WITH s AS (
-                SELECT "id" FROM %1$s AS t LEFT JOIN "pg_catalog"."pg_locks" AS l ON "locktype" OPERATOR(pg_catalog.=) 'userlock' AND "mode" OPERATOR(pg_catalog.=) 'AccessExclusiveLock' AND "granted" AND "objsubid" OPERATOR(pg_catalog.=) 4 AND "database" OPERATOR(pg_catalog.=) %2$i AND "classid" OPERATOR(pg_catalog.=) "id">>32 AND "objid" OPERATOR(pg_catalog.=) "id"<<32>>32
+                SELECT "id" FROM %1$s AS t LEFT JOIN "pg_catalog"."pg_locks" AS l ON "locktype" OPERATOR(pg_catalog.=) 'userlock' AND "mode" OPERATOR(pg_catalog.=) 'AccessExclusiveLock' AND "granted" AND "objsubid" OPERATOR(pg_catalog.=) 4 AND "database" OPERATOR(pg_catalog.=) %2$i AND "classid" OPERATOR(pg_catalog.=) "id" OPERATOR(pg_catalog.>>) 32 AND "objid" OPERATOR(pg_catalog.=) "id"<<32 OPERATOR(pg_catalog.>>) 32
                 WHERE "state" IN ('TAKE', 'WORK') AND l.pid IS NULL FOR UPDATE OF t %3$s
             ) UPDATE %1$s AS t SET "state" = 'PLAN', "start" = NULL, "stop" = NULL, "pid" = NULL FROM s WHERE t.id OPERATOR(pg_catalog.=) s.id RETURNING t.id
         ), work.schema_table, work.shared->oid,
@@ -278,7 +278,7 @@ static void work_timeout(void) {
         appendStringInfo(&src, SQL(
            SELECT COALESCE(LEAST(EXTRACT(epoch FROM ((
                 SELECT GREATEST("plan" + pg_catalog.current_setting('pg_task.reset')::pg_catalog.interval - CURRENT_TIMESTAMP, '0 sec'::pg_catalog.interval) AS "plan" FROM %1$s AS t
-                LEFT JOIN "pg_catalog"."pg_locks" AS l ON "locktype" = 'userlock' AND "mode" = 'AccessExclusiveLock' AND "granted" AND "objsubid" = 4 AND "database" = %2$i AND "classid" = "id">>32 AND "objid" = "id"<<32>>32
+                LEFT JOIN "pg_catalog"."pg_locks" AS l ON "locktype" = 'userlock' AND "mode" = 'AccessExclusiveLock' AND "granted" AND "objsubid" = 4 AND "database" = %2$i AND "classid" = "id" OPERATOR(pg_catalog.>>) 32 AND "objid" = "id"<<32 OPERATOR(pg_catalog.>>) 32
                 WHERE "state" IN ('TAKE', 'WORK') AND l.pid IS NULL ORDER BY 1 LIMIT 1
            )))::pg_catalog.int8 * 1000, EXTRACT(epoch FROM ((
                 SELECT "plan" + pg_catalog.concat_ws(' ', (-CASE WHEN "max" >= 0 THEN 0 ELSE "max" END)::pg_catalog.text, 'msec')::pg_catalog.interval - CURRENT_TIMESTAMP AS "plan" FROM %1$s WHERE "state" = 'PLAN' AND "plan" + pg_catalog.concat_ws(' ', (-CASE WHEN "max" >= 0 THEN 0 ELSE "max" END)::pg_catalog.text, 'msec')::pg_catalog.interval >= CURRENT_TIMESTAMP ORDER BY 1 LIMIT 1
