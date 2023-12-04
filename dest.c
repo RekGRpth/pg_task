1d14d76cfee69ccd94cced2fb865bd016d8c1d74
RekGRpth <rekgrpth@gmail.com>
2023-12-04 14:35:17 +0500

    rename

--- dest.c
+++ dest.c
@@ -208,22 +208,22 @@ static void dest_ungrab(grub_t *grub, StringInfo buf) {
 
 bool dest_timeout(void) {
     int StatementTimeoutMy = StatementTimeout;
-    grub_t err;
-    grub_t out;
+    grub_t error;
+    grub_t output;
     if (task_work(&task)) return true;
     elog(DEBUG1, "id = %li, timeout = %i, input = %s, count = %i", task.shared->id, task.timeout, task.input, task.count);
     set_ps_display_my("timeout");
     StatementTimeout = task.timeout;
-    dest_grab(&err, stderr);
-    dest_grab(&out, stdout);
+    dest_grab(&error, stderr);
+    dest_grab(&output, stdout);
     PG_TRY();
         if (!task.active) ereport(ERROR, (errcode(ERRCODE_QUERY_CANCELED), errmsg("task not active")));
         dest_execute();
     PG_CATCH();
         dest_catch();
     PG_END_TRY();
-    dest_ungrab(&err, &task.error);
-    dest_ungrab(&out, &task.output);
+    dest_ungrab(&error, &task.error);
+    dest_ungrab(&output, &task.output);
     StatementTimeout = StatementTimeoutMy;
     pgstat_report_stat(false);
     pgstat_report_activity(STATE_IDLE, NULL);
