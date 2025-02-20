1575abb20b9aaf4a269b2350264f2343e98f0e73
RekGRpth <rekgrpth@gmail.com>
2025-02-20 15:14:01 +0500

    else

--- work.c
+++ work.c
@@ -974,9 +974,9 @@ void work_main(Datum main_arg) {
         for (int i = 0; i < nevents; i++) {
             WaitEvent *event = &events[i];
             if (event->events & WL_POSTMASTER_DEATH) ShutdownRequestPending = true;
-            if (event->events & WL_LATCH_SET) work_latch();
-            if (event->events & WL_SOCKET_READABLE) work_readable(event->user_data);
-            if (event->events & WL_SOCKET_WRITEABLE) work_writeable(event->user_data);
+            else if (event->events & WL_LATCH_SET) work_latch();
+            else if (event->events & WL_SOCKET_READABLE) work_readable(event->user_data);
+            else if (event->events & WL_SOCKET_WRITEABLE) work_writeable(event->user_data);
         }
         INSTR_TIME_SET_CURRENT(current_time_reset);
         INSTR_TIME_SUBTRACT(current_time_reset, start_time_reset);
