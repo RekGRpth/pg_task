917ee38cd6a4f76bfb204cfd4494ee823f06c27a
RekGRpth <rekgrpth@gmail.com>
2025-02-20 10:42:19 +0500

    rename

--- init.c
+++ init.c
@@ -353,11 +353,11 @@ bool is_log_level_output(int elevel, int log_min_level) {
     return false;
 }
 
-int init_bgw_main_arg(Shared *ws) {
+int init_bgw_main_arg(Shared *s) {
     LWLockAcquire(BackgroundWorkerLock, LW_EXCLUSIVE);
     for (int slot = 0; slot < max_worker_processes; slot++) if (!shared[slot].in_use) {
         pg_write_barrier();
-        shared[slot] = *ws;
+        shared[slot] = *s;
         shared[slot].in_use = true;
         LWLockRelease(BackgroundWorkerLock);
         elog(DEBUG1, "slot = %i", slot);
