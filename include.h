e0f76409fe534726a7350f8a9a056101fdca200f
RekGRpth <rekgrpth@gmail.com>
2025-02-22 10:24:17 +0500

    sort

--- include.h
+++ include.h
@@ -170,6 +170,7 @@ Portal SPI_cursor_open_my(const char *src, SPIPlanPtr plan, Datum *values, const
 Portal SPI_cursor_open_with_args_my(const char *src, int nargs, Oid *argtypes, Datum *values, const char *nulls, bool read_only);
 Shared *init_shared(Datum main_arg);
 SPIPlanPtr SPI_prepare_my(const char *src, int nargs, Oid *argtypes);
+Task *get_task(void);
 void appendBinaryStringInfoEscapeQuote(StringInfo buf, const char *data, int len, bool string, char escape, char quote);
 void append_with_tabs(StringInfo buf, const char *str);
 void init_conf(bool dynamic);
@@ -184,7 +185,5 @@ void SPI_execute_with_args_my(const char *src, int nargs, Oid *argtypes, Datum *
 void SPI_finish_my(void);
 void task_error(ErrorData *edata);
 void task_free(Task *t);
-Task *get_task(void);
-
 
 #endif // _INCLUDE_H_
